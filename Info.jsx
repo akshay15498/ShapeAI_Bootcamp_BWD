a3371651faacff31886bc77ec832da106f729a37
Akshay Sudarshan Deshpande <84176670+akshay15498@users.noreply.github.com>
2021-06-27 11:25:01 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.Js</h1>
+      <p>A web development React.Js and Javascript</p>
+    </div>
+  );
+}
+export default Info;
